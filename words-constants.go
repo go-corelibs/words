cbfaf7a397b42064551688cd955618aff6b5081e
Kevin C. Krinke <kevin@krinke.ca>
2024-01-05 10:20:46 -0500

    words: refactored Config into Words struct; rewrote Range to be more accurate, no longer drops non-charsets words; added wpm settings; added new Metrics method, returning ReadingMetrics struct; code cosmetic changes and reorganizations

---
+++ words-constants.go
@@ -0,0 +1,23 @@
+// Copyright (c) 2024  The Go-Curses Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package words
+
+const (
+	// AverageWordsPerMinute is the words per minute read by average adults
+	AverageWordsPerMinute = 238.0
+	// RelaxedWordsPerMinute is an estimation of words per minute read by
+	// older children and tired adults looking at monitors and screens all day
+	RelaxedWordsPerMinute = 177.0
+)
--- words.go
+++ words.go
@@ -12,123 +12,208 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package words provides a means for counting the numbers of words and
+// estimating the reading time of the given content.
+//
+// Words per minute values based on:
+//
+//	https://irisreading.com/what-is-the-average-reading-speed/
+//	https://www.researchgate.net/publication/332380784_How_many_words_do_we_read_per_minute_A_review_and_meta-analysis_of_reading_rate
+//	https://www.sciencedirect.com/science/article/abs/pii/S0749596X19300786
+//
+// Package words was inspired by:
+//
+//	https://github.com/byn9826/words-count/blob/master/src/globalWordsCount.js
 package words
 
 import (
 	"regexp"
 	"strings"
-)
+	"time"
 
-// inspired by https://github.com/byn9826/words-count/blob/master/src/globalWordsCount.js
+	"github.com/go-corelibs/maths"
+	"github.com/go-corelibs/slices"
+)
 
 var (
-	DefaultPunctuation = []rune{
-		',', '，', '.', '。', ':', '：', ';', '；', '[', ']', '【', ']', '】', '{', '｛', '}', '｝',
-		'(', '（', ')', '）', '<', '《', '>', '》', '$', '￥', '!', '！', '?', '？', '~', '～',
-		'\'', '’', '"', '“', '”',
-		'*', '/', '\\', '&', '%', '@', '#', '^', '、', '、', '、', '、',
-	}
-
-	RxSpaces   = regexp.MustCompile(`\s+`)
-	RxSymbols  = regexp.MustCompile(`\p{S}`)
-	RxCharSets = regexp.MustCompile(`[\p{Han}\p{Katakana}\p{Hiragana}\p{Hangul}]`)
+	rxSpaces   = regexp.MustCompile(`\s+`)
+	rxSymbols  = regexp.MustCompile(`\p{S}`)
+	rxCharSets = regexp.MustCompile(`[\p{Han}\p{Katakana}\p{Hiragana}\p{Hangul}]`)
 )
 
-type Config struct {
-	PunctuationAsBreaker      bool
+// Words is the definition for running customized word operations and is the
+// implementation driving the normal package functions
+type Words struct {
+	// PunctuationAsBreaker specifies that punctuation characters should not
+	// be removed and instead be replaced with a space. For example, "they're"
+	// by default is collapsed to "theyre" for counting purposes. With
+	// PunctuationAsBreaker set to true, "they're" would become "they re"
+	PunctuationAsBreaker bool
+	// DisableDefaultPunctuation specifies that only the Words.Punctuation
+	// runes are to be considered punctuation
 	DisableDefaultPunctuation bool
-	Punctuation               []rune
+	// Punctuation defines the list of punctuation runes to use when parsing
+	// words out of content
+	Punctuation []rune
+	// AverageWPM specifies the average words per minute to use
+	// for calculating Metrics, default is 238.0, see: AverageWordsPerMinute
+	AverageWPM float64
+	// RelaxedWPM specifies the average words per minute to use
+	// for calculating Metrics, default is 177.0, see: RelaxedWordsPerMinute
+	RelaxedWPM float64
+
+	combined []rune
 }
 
-func DefaultConfig() (cfg *Config) {
-	return &Config{}
+// Default returns a new Words instance configured with sane defaults
+func Default() (w *Words) {
+	return &Words{
+		AverageWPM: AverageWordsPerMinute,
+		RelaxedWPM: RelaxedWordsPerMinute,
+	}
 }
 
-func (cfg *Config) List(input string) (list []string) {
+func (w *Words) prepare() {
+	if w.AverageWPM <= 0 {
+		w.AverageWPM = AverageWordsPerMinute
+	}
+	if w.RelaxedWPM <= 0 {
+		w.RelaxedWPM = RelaxedWordsPerMinute
+	}
+
+	if !w.DisableDefaultPunctuation {
+		w.combined = append(w.combined, DefaultPunctuation...)
+	}
+	if len(w.Punctuation) > 0 {
+		w.combined = append(w.combined, w.Punctuation...)
+	}
+	w.combined = slices.Unique(w.combined)
+	return
+}
+
+// List returns a list of all the words detected within the given input that
+// are separated by spaces, word characters not separated by spaces are
+// clumped within individual items of the list returned. Use Words.Parse to
+// derive a more accurate word list
+func (w *Words) List(input string) (list []string) {
+	w.prepare()
+
 	var work, spacer string
 	if work = strings.TrimSpace(input); work == "" {
 		return
-	} else if cfg.PunctuationAsBreaker {
+	} else if w.PunctuationAsBreaker {
 		spacer = " "
 	}
 
-	var combined []rune
-	if !cfg.DisableDefaultPunctuation {
-		combined = append(combined, DefaultPunctuation...)
-	}
-	if len(cfg.Punctuation) > 0 {
-		combined = append(combined, cfg.Punctuation...)
-	}
-
 	// replace all punctuation with spacers
-	if len(combined) > 0 {
-		for _, r := range combined {
+	if len(w.combined) > 0 {
+		for _, r := range w.combined {
 			work = strings.ReplaceAll(work, string(r), spacer)
 		}
 	}
 
-	work = RxSymbols.ReplaceAllString(work, "")
-	work = RxSpaces.ReplaceAllString(work, " ")
+	work = rxSymbols.ReplaceAllString(work, "")
+	work = rxSpaces.ReplaceAllString(work, " ")
 	work = strings.TrimSpace(work)
 
 	list = strings.Split(work, " ")
 	return
 }
 
-func (cfg *Config) Range(input string, fn func(word string)) {
-	list := cfg.List(input)
+// Range iterates over all words detected within input, calling the given `fn`
+// for each word found
+func (w *Words) Range(input string, fn func(word string)) {
+	w.prepare()
+
+	list := w.List(input)
 
 	for _, word := range list {
-		var carry []string
-		if RxCharSets.MatchString(word) {
+		if rxCharSets.MatchString(word) {
+
+			// handle mixed-locales
+			var latin string
+			var carry []string
 			for _, r := range word {
-				if char := string(r); RxCharSets.MatchString(char) {
+				if char := string(r); rxCharSets.MatchString(char) {
+					if latin != "" {
+						carry = append(carry, latin)
+						latin = ""
+					}
 					carry = append(carry, char)
+				} else if !slices.Within(r, w.combined) {
+					latin += char
 				}
 			}
-		}
-		if carried := len(carry); carried == 0 {
-			fn(word)
-		} else {
-			for _, w := range carry {
-				fn(w)
+			if latin != "" {
+				carry = append(carry, latin)
+				latin = ""
 			}
+			for _, carriedWord := range carry {
+				fn(carriedWord)
+			}
+
+		} else {
+
+			fn(word)
+
 		}
 	}
 
 }
 
-func (cfg *Config) Count(input string) (count int) {
-	cfg.Range(input, func(_ string) {
+// Count returns the total number of words detected within the given input
+func (w *Words) Count(input string) (count int) {
+	w.Range(input, func(_ string) {
 		count += 1
 	})
 	return
 }
 
-func (cfg *Config) Parse(input string) (words []string) {
-	cfg.Range(input, func(word string) {
+// Parse returns the total list of words detected within the given input
+func (w *Words) Parse(input string) (words []string) {
+	w.Range(input, func(word string) {
 		words = append(words, word)
 	})
 	return
 }
 
-func (cfg *Config) Search(query, content string) (score int, found []string) {
-	keywords := cfg.Parse(strings.ToLower(query))
+// Search performs a case-insensitive search for the keywords within the given
+// `query` string and returns the list of unique query keywords found along
+// with a simple scoring metric weighing earlier keywords more than later
+// keywords
+func (w *Words) Search(query, content string) (score int, found []string) {
+	w.prepare()
+
+	keywords := w.Parse(strings.ToLower(query))
 	keywordCount := len(keywords)
-	haystack := cfg.Parse(strings.ToLower(content))
-	unique := map[string]struct{}{}
+	haystack := w.Parse(strings.ToLower(content))
 
 	for _, word := range haystack {
 		for idx, keyword := range keywords {
 			if word == keyword {
 				weight := keywordCount - idx
 				score += weight
-				if _, present := unique[word]; !present {
-					found = append(found, word)
-				}
+				found = append(found, word)
 			}
 		}
 	}
 
+	found = slices.Unique(found)
+	return
+}
+
+// Metrics gets the Words.Count and derives some estimated reading times
+func (w *Words) Metrics(content string) (m ReadingMetrics) {
+	w.prepare()
+
+	m.WordCount = w.Count(content)
+
+	avgTime := float64(m.WordCount) / w.AverageWPM
+	m.Average.Minutes = maths.RoundDown(avgTime)
+	m.Average.Duration = time.Duration(avgTime * float64(time.Minute))
+
+	relTime := float64(m.WordCount) / w.RelaxedWPM
+	m.Relaxed.Minutes = maths.RoundUp(relTime)
+	m.Relaxed.Duration = time.Duration(relTime * float64(time.Minute))
 	return
 }
